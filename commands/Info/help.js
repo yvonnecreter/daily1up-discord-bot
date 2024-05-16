48109f751b8a88d370ce82a32f5609ec7e1cdd0e
ycy <yvonnella.keks@gmail.com>
2024-05-16 19:30:45 +0200

    Update Help Command

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -7,13 +7,14 @@ module.exports = {
         .setDescription('How do I start?'),
     async execute(interaction, client) {
         const instructionsEmbed = new EmbedBuilder()
-            .setColor('#5865F2') // Official Discord Blurple color
+            .setColor('#5865F2')
             .setTitle('✨ Daily Uplift Setup')
             .setDescription('Get started with daily uplifts in just a few steps:')
             .addFields(
                 { name: '🌎 Set Your Timezone', value: 'Use the `/settimezone` command to ensure reminders arrive at the right time.' },
                 { name: '⏰ Add Message Times', value: 'Use the `/addtime` command to specify when you want to receive 1-ups.' },
-                { name: '😊 Get an Uplift', value: 'Need a quick boost? Use `/senduplift` for an instant motivational message.' },
+                { name: '📥 Get an Uplift', value: 'Need a quick boost? Use `/senduplift` for an instant motivational message.' },
+                { name: '⚙️ Message Preferences', value: 'Use the `/settings` command to choose the type of message you want to receive!' },
                 { name: '🔕 Remove Notifications', value: 'Change your mind? Use `/removetime` to remove any scheduled 1-ups.' }
             )
             .setFooter({ text: 'You\'re all set!' });
